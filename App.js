0aaa5b10a21ffdabee2ac6d0bdc3912ac7a9bcaa
YashPatil2001 <yashpatilc2001@gmail.com>
2022-05-25 18:03:23 +0530

    user-input

--- App.js
+++ App.js
@@ -6,46 +6,50 @@
  * @flow strict-local
  */
 
- import React,{useState} from 'react';
- import { StyleSheet, Text, View, Button} from 'react-native';
- 
- export default function App() {
-   const [name,setName] = useState('Yash')
-   const [person,setPerson] = useState({ name:'Dip Patil' , age:20})
-   
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
-  const onUpdate = () => {
-     setName('Madhu')
-   }
- 
- const onUpdatePerson = () => {
-   setPerson({
-     name: 'Aryaa',
-     age: '30'
-   })
- }  
-   return (
-     <View style={styles.container}>
-        <Text>My Name is {name}</Text>
-        <Text>Hist Name is {person.name} and his age is {person.age}</Text>
-        <View style={styles.btnContainer}>
-           <Button title='Update name' onPress={onUpdate}/>
-        </View>
-        <View style={styles.btnContainer}>
-           <Button title='Set person' onPress={onUpdatePerson}/>
-        </View>
-     </View>
-   );
- }
- 
- const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     backgroundColor: '#fff',
-     alignItems: 'center',
-     justifyContent: 'center',
-   },
-   btnContainer: {
-       marginTop: 20
-   }
- });
+export default function App() {
+  const [name, setName] = useState('Yash');
+  const [age, setAge] = useState(10);
+
+
+  return (
+    <View style={styles.container}>
+
+      <Text>Enter Name:</Text>
+      <TextInput style={styles.input}
+                 placeholder="e.g.Indira"
+                 keyboardType='text'
+                 onChangeText={(val) => setName(val)}/>
+      
+      <Text>Enter Age:</Text>
+      <TextInput style={styles.input}
+                 placeholder="e.g.20" 
+                 keyboardType='numeric'
+                 onChangeText={(val) => setAge(val)} />
+      <Text>
+        Name : {name} Age : {age}
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnContainer: {
+    marginTop: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+  },
+});
